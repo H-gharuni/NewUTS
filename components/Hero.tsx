1011b7f078c28bda4ab453424b3f77e857e1ae4c
Hossein <gharuni@gmail.com>
2025-10-26 20:21:49 +0000

    Fix hero section bottom border: add -mb-px to wave overlay

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -60,13 +60,14 @@ export default function Hero() {
       </div>
 
       {/* Decorative bottom wave */}
-      <div className="absolute bottom-0 left-0 right-0 w-full">
+      <div className="absolute bottom-0 left-0 right-0 w-full -mb-px">
         <svg
           viewBox="0 0 1440 120"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           className="w-full h-12 sm:h-20 block"
           preserveAspectRatio="none"
+          style={{ display: 'block' }}
         >
           <path
             d="M0 120L60 105C120 90 240 60 360 45C480 30 600 30 720 37.5C840 45 960 60 1080 67.5C1200 75 1320 75 1380 75L1440 75V120H1380C1320 120 1200 120 1080 120C960 120 840 120 720 120C600 120 480 120 360 120C240 120 120 120 60 120H0Z"
